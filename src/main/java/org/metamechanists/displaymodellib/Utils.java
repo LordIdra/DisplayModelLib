360fd5049b6cbb2e20e7edb90188e26238f5f13b
MetaMechanists <percy.baker.0@gmail.com>
2023-08-21 14:17:57 +0100

    Remove debug message

--- src/main/java/org/metamechanists/displaymodellib/Utils.java
+++ src/main/java/org/metamechanists/displaymodellib/Utils.java
@@ -7,7 +7,6 @@ import org.bukkit.Bukkit;
 @UtilityClass
 public class Utils {
     public int getMajorServerVersion() {
-        Bukkit.getLogger().info(Bukkit.getServer().getBukkitVersion());
         return Integer.parseInt(Bukkit.getServer().getBukkitVersion().split("\\.")[1]);
     }
 }
